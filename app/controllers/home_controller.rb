7b068bb893fa07dabc207ba17c2b03217d984376
RicardoRS <ricardors94@hotmail.com>
2017-04-07 13:04:41 -0500

    Added Content generation, linked to static pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @content1 = Content.find(1)
+    @content2 = Content.find(2)
+    @content3 = Content.find(3)
+    @content4 = Content.find(4)
+    @content5 = Content.find(5)
+    @content6 = Content.find(6)
   end
 
   def about
+    @content1 = Content.find(8)
   end
 
   def productos
